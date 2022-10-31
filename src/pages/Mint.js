ea6af36216197e9673e30aca16456e59b7137873
muhammadalighouri <98492348+muhammadalighouri@users.noreply.github.com>
2022-10-31 18:39:06 +0500

    asdadasd

--- src/pages/Mint.js
+++ src/pages/Mint.js
@@ -93,7 +93,7 @@ const Mint = () => {
             </section>
             <Steps />
             <AboutNft />
-            <Holder/>
+            {/* <Holder/> */}
             <Marchendise />
             <Footer />
         </>
